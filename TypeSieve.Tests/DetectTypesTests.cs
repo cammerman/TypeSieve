1c38502a79cca4ce6a93042d96122f6420fd8a6c
Chris Ammerman <cammerman@gmail.com>
2011-08-14 21:35:28 -0500

    Small test name & implementation improvement.

--- TypeSieve.Tests/DetectTypesTests.cs
+++ TypeSieve.Tests/DetectTypesTests.cs
@@ -11,27 +11,36 @@ namespace TypeSieve.Tests
 	public class DetectTypesTests
 	{
 		[Fact]
-		public void FromAssemblyContaining_T_StoresAssembly()
+		public void KnownTypes_GivenFromAssemblyContainingHasBeenCalled_ReturnsTypesInAssemlby()
 		{
 			var subject = new DetectTypes();
 
 			subject.FromAssemblyContaining<IService>();
 
-			Assert.True(
-				subject.KnownTypes()
-					.Contains(typeof(IService)));
+			var expectedTypes = typeof(IService).Assembly.GetTypes();
+			var knownTypes = subject.KnownTypes();
+
+			foreach (var type in expectedTypes)
+				Assert.Contains(
+					type,
+					knownTypes);
 		}
 
 		[Fact]
-		public void FromNamespaceContaining_T_StoresAssembly()
+		public void KnownTypes_GivenFromNamespaceContainingHasBeenCalled_ReturnsTypesInAssembly()
 		{
 			var subject = new DetectTypes();
 
 			subject.FromNamespaceContaining<IService>();
+			
+			var ns = typeof(IService).Namespace;
+			var expectedTypes = typeof(IService).Assembly.GetTypes().Where(type => type.Namespace == ns);
+			var knownTypes = subject.KnownTypes();
 
-			Assert.True(
-				subject.KnownTypes()
-					.Contains(typeof(IService)));
+			foreach (var type in expectedTypes)
+				Assert.Contains(
+					type,
+					knownTypes);
 		}
 
 		[Fact]
